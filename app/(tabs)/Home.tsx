19a6d73e8eec02f11925d68012e9490b4ec2b5ee
Estifanos58 <estifkebe08@gmail.com>
2025-05-01 21:06:26 +0300

    update: btns in the channel reconfigred

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,29 +1,59 @@
-import { StyleSheet, Text, View } from "react-native"
-import { SharedProfile } from "../components/SharedProfile"
-import { Header } from "../components/Header"
-import ChannelName from "../components/ChannelName"
+import { StyleSheet, Text, View } from "react-native";
+import { SharedProfile } from "../components/SharedProfile";
+import { Header } from "../components/Header";
+import ChannelName from "../components/ChannelName";
+import { useState } from "react";
 
-const  Home  = () => {
-    return (
-        <>
-        <View>
-            <SharedProfile/>
-            <View style={styles.mainContainer}>
-                <Header title={"channels"}/>
-                <ChannelName title={"general"} description={"general channel"} notfication={2}/>
-                <ChannelName title={"marketing"} description={"Company discussion"} />
-                <Header title="direct Messages"/>
+const Home = () => {
+  const channels = [
+    {
+      id: 1,
+      name: "general",
+      description: "Latest updated from the team is here",
+      notification: 3,
+    },
+    {
+      id: 2,
+      name: "marketing",
+      description: "Company discussion",
+      notification: 0,
+    },
+  ];
 
-            </View>
+  const [selected, setSelected] = useState(channels[0].id);
+  return (
+    <>
+      <View>
+        <SharedProfile />
+        <View style={styles.mainContainer}>
+          <Header title={"channels"} />
+          <View style={styles.channelsContainer}>
+            {channels.map((channel) => (
+              <ChannelName
+                selected={selected}
+                setSelected={setSelected}
+                key={channel.id}
+                channel={channel}
+              />
+            ))}
+          </View>
+          <Header title="direct Messages" />
         </View>
-        </>
-    )
-}
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        padding: 10
-    }
-})
+  mainContainer: {
+    padding: 10,
+  },
+  channelsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+    marginVertical: 20,
+  },
+});
 
-export default Home
+export default Home;
--- app/components/ChannelName.tsx
+++ app/components/ChannelName.tsx
@@ -1,70 +1,101 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const ChannelName = ({title, description, notfication}:{title:string, description:string, notfication?: number}) => {
-    return (
-        <View style={styles.channelNameContainer}> 
-            <View style={styles.groupLeft}>
-                <Text style={styles.icon} >#</Text>
-                <View>
-                    <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.description}>{description}</Text>
-                </View>
-            </View>
-            { notfication && <View style={styles.notification}>
-                <Text style={styles.notText}>{notfication}</Text>
-            </View>}
+const ChannelName = ({
+  selected,
+  setSelected,
+  channel,
+}: {
+    selected:number,
+    setSelected: any,
+    channel: any
+}) => {
+  return (
+    <TouchableOpacity onPress={()=>setSelected(channel.id)} style={selected === channel.id ? styles.selectedContainer : styles.channelNameContainer}>
+      <View style={styles.groupLeft}>
+        <Text style={styles.icon}>#</Text>
+        <View>
+          <Text  style={selected === channel.id ? styles.selectedTitle :styles.title}>{channel.name}</Text>
+          <Text style={selected === channel.id ? styles.selectedDesc:styles.description}>
+            {channel.description.length > 25
+              ? channel.description.substring(0, 30) + "..."
+              : channel.description}
+          </Text>
         </View>
-    )
-}
+      </View>
+      {channel.notification && (
+        <View style={styles.notification}>
+          <Text style={styles.notText}>{channel.notification}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    channelNameContainer: {
-        display:'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding:10,
-        paddingHorizontal: 20,
-        backgroundColor: '#fff',
-        borderRadius: 20,
-        borderLeftWidth: 4,
-        borderLeftColor: 'orange',
-        marginVertical: 15
-    },
-    groupLeft: {
-        display: 'flex',
-        flexDirection: 'row',
-        gap: 7,
-        alignItems: 'center'
-    },
-    icon: {
-        fontSize: 20,
-        fontWeight: '700'
-    },
-    title: {
-        fontSize: 16,
-        fontWeight: '800'
-    },
-    description: {
-        fontSize: 13,
-        fontWeight: '300',
-        color: 'gray'
-    },
-    notification: {
-        backgroundColor: 'orange',
-        borderRadius: 50,
-        display:'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 40,
-        height: 40,
-    },
-    notText: {
-        color: '#fff',
-        fontSize: 20
-    }
-
-})
+  channelNameContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+  },
+  selectedContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    borderLeftWidth: 4,
+    borderLeftColor: "orange",
+  },
+  groupLeft: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 7,
+    alignItems: "center",
+  },
+  icon: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  selectedTitle: {
+    fontSize: 16,
+    fontWeight: "800",
+  },
+  selectedDesc: {
+    fontSize: 13,
+    fontWeight: "300",
+    color: 'gray'
+  },
+  description: {
+    fontSize: 12,
+    fontWeight: "200",
+    color: "gray",
+  },
+  notification: {
+    backgroundColor: "orange",
+    borderRadius: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 35,
+    height: 35,
+  },
+  notText: {
+    color: "#fff",
+    fontSize: 20,
+  },
+});
 
 export default ChannelName;
